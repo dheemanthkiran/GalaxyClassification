ac36e4cc68e52fa7acd1fcf21f072180f45637e7
Dheemanth Kiran <ge47dan@tum.de>
2023-01-23 12:44:44 +0100

    Training_Loop added

--- Training_Loop.py
+++ Training_Loop.py
@@ -63,9 +63,40 @@ def train_one_epoch(epoch_index, tb_writer):
 
 epochsNum = 10
 
-
 for epoch in range(epochsNum):
-    print("Epoch {}:".format(epoch))
+    print('EPOCH {}:'.format(epochsNum + 1))
+
+    # Make sure gradient tracking is on, and do a pass over the data
+    model.train(True)
+    avg_loss = train_one_epoch(epochsNum, writer)
+
+    # We don't need gradients on to do reporting
+    model.train(False)
+
+    running_vloss = 0.0
+    for i, vdata in enumerate(validationLoader):
+        vinputs, vlabels = vdata
+        voutputs = model(vinputs)
+        vloss = loss_fn(voutputs, vlabels)
+        running_vloss += vloss
+
+    avg_vloss = running_vloss / (i + 1)
+    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+    # Log the running loss averaged per batch
+    # for both training and validation
+    writer.add_scalars('Training vs. Validation Loss',
+                       {'Training': avg_loss, 'Validation': avg_vloss},
+                       epochsNum + 1)
+    writer.flush()
+
+    # Track best performance, and save the model's state
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
+        model_path = 'model_{}_{}'.format(timestamp, epochsNum)
+        torch.save(model.state_dict(), model_path)
+
+    epochsNum += 1
 
 
 
